2bda92c6bad30da38229e08d711cebf1ec382b59
蘇宥羿 <youyi@suyouyideMacBook-Air.local>
2025-06-02 03:00:20 +0800

    Initial commit: 加密貨幣槓桿計算器

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    icon: path.join(__dirname, 'icon.png')
+  })
+
+  win.loadFile('index.html')
+  
+  // 移除菜單欄
+  win.setMenuBarVisibility(false)
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+}) 
